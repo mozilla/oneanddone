4102e1c97bcc78c09cf261da9a75ddb0d118a1b2
Michael Kelly <mkelly@mozilla.com>
2013-11-10 22:39:35 -0500

    Do not use rel="stylesheet/less" when TEMPLATE_DEBUG is False.

--- oneanddone/base/helpers.py
+++ oneanddone/base/helpers.py
@@ -1,5 +1,7 @@
+from django.conf import settings
+
 from jingo import register
-from jingo_minify.helpers import get_css_urls
+from jingo_minify.helpers import css, get_css_urls
 from jinja2 import Markup
 
 
@@ -7,8 +9,12 @@ from jinja2 import Markup
 def less_css(bundle):
     """
     Similar to jingo_minify's css helper, but uses rel="stylesheet/less"
-    instead of rel="stylesheet".
+    instead of rel="stylesheet" when TEMPLATE_DEBUG is True. If
+    TEMPLATE_DEBUG is False, this passes the call on to the css helper.
     """
+    if not settings.TEMPLATE_DEBUG:
+        return css(bundle)
+
     urls = get_css_urls(bundle)
     link_tag = '<link rel="stylesheet/less" media="screen,projection,tv" href="{0}" />'
     return Markup('\n'.join([link_tag.format(url) for url in urls]))
