f94bdbdf9de61351775d0a4c9d9725258be3ba8a
Michael Kelly <mkelly@mozilla.com>
2013-12-20 10:01:02 -0500

    Redirect logged-in users from the homepage to the profile page.

--- oneanddone/base/views.py
+++ oneanddone/base/views.py
@@ -2,7 +2,14 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from django.views import generic
+from django.shortcuts import redirect
 
 
 class HomeView(generic.TemplateView):
     template_name = 'base/home.html'
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated():
+            return redirect('users.profile.detail')
+        else:
+            return super(HomeView, self).get(*args, **kwargs)
