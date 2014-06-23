a695638c506a03785931aa5e595a92d5fa1da5ed
Bob Silverberg <bob.silverberg@gmail.com>
2014-06-23 17:41:36 -0400

    Bug 1020611 - Update Available tasks page with new filter

--- oneanddone/tasks/tests/test_filters.py
+++
@@ -1,9 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from nose.tools import eq_
-
-from oneanddone.base.tests import TestCase
-from oneanddone.tasks.filters import AvailableTasksFilterSet
-from oneanddone.tasks.tests import TaskFactory
-
