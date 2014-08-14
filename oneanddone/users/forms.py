3775ccd6d6acee5fc13440961687a6bb97a7ac94
Bob Silverberg <bob.silverberg@gmail.com>
2014-08-14 17:03:20 -0400

    Bug 1033475 - Allow user to configure email preferences from their profile
    Update User Profile edit screen to allow email preference
    Update text on account creation screen to indicate how to remove consent to email
    Don't send email address in feedback email if they have not consented
    Hide user email from most admin screens if they have not consented to receive email
    Add consent_to_email field to UserProfile
    Change OneAndDoneUserManager.get_query_set() to from select_related to prefetch_related due to https://code.djangoproject.com/ticket/15040
    
    Should secure Auth - User admin to superusers only so that not everyone can see all the email addresses
    
    Post review: Reduce length of very long string in SignUpForm

--- oneanddone/users/forms.py
+++ oneanddone/users/forms.py
@@ -7,7 +7,11 @@ from oneanddone.users.models import UserProfile
 
 class SignUpForm(forms.ModelForm):
     pp_checkbox = forms.BooleanField(
-        label=_("You are creating a profile that will include a public username and work history. You will begin to receive email communications from Mozilla once you have completed tasks. You may unsubscribe at anytime by clicking the line at the bottom of these emails."),
+        label=_("You are creating a profile that will include a public username"
+                " and work history."
+                " You will begin to receive email communications from Mozilla"
+                " once you have completed tasks."
+                " You may configure email preferences when editing your profile."),
         required=True,
         initial=True)
     username = forms.RegexField(
@@ -30,7 +34,8 @@ class UserProfileForm(forms.ModelForm):
         label=_("Username:"),
         max_length=30, regex=r'^[a-zA-Z0-9]+$',
         error_messages={'invalid': _("This value may contain only alphanumeric characters.")})
+    consent_to_email = forms.BooleanField(required=False)
 
     class Meta:
         model = UserProfile
-        fields = ('name', 'username')
+        fields = ('name', 'username', 'consent_to_email')
