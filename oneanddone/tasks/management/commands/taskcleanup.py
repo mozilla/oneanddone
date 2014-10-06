02de9a6092549e1310adba9fd844cb9c67958d70
Bob Silverberg <bob.silverberg@gmail.com>
2014-10-06 14:42:47 -0400

    Add timestamps to management task output

--- oneanddone/tasks/management/commands/taskcleanup.py
+++ oneanddone/tasks/management/commands/taskcleanup.py
@@ -1,6 +1,8 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from datetime import datetime
+
 from django.core.management.base import BaseCommand
 
 from oneanddone.tasks.models import Task, TaskAttempt
@@ -11,8 +13,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         invalidated = Task.invalidate_tasks()
-        self.stdout.write('%s tasks were invalidated via bug data\n' % invalidated)
+        self.stdout.write('%s: %s tasks were invalidated via bug data\n' %
+                          (datetime.now().isoformat(), invalidated))
         closed = TaskAttempt.close_stale_onetime_attempts()
-        self.stdout.write('%s stale one-time attempts were closed\n' % closed)
+        self.stdout.write('%s: %s stale one-time attempts were closed\n' %
+                          (datetime.now().isoformat(), closed))
         closed = TaskAttempt.close_expired_task_attempts()
-        self.stdout.write('%s attempts for expired tasks were closed\n' % closed)
+        self.stdout.write('%s: %s attempts for expired tasks were closed\n' %
+                          (datetime.now().isoformat(), closed))
