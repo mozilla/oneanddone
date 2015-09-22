b14d126ae23f17c5672ec5fc417c84d911436b4c
Bob Silverberg <bob.silverberg@gmail.com>
2015-09-22 09:20:01 -0400

    Update newrelic integration to work with Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uwsgi uwsgi.ini
+web: newrelic-admin run-program uwsgi --ini uwsgi.ini
--- oneanddone/wsgi.py
+++ oneanddone/wsgi.py
@@ -13,7 +13,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oneanddone.settings')
 from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
-import newrelic.agent
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
@@ -22,13 +21,6 @@ application = DjangoWhiteNoise(application)
 # Add media files
 if settings.MEDIA_ROOT and settings.MEDIA_URL:
     application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
-
-# Add NewRelic
-NEW_RELIC_LICENSE_KEY = os.environ.get('NEW_RELIC_LICENSE_KEY', False)
-NEW_RELIC_APP_NAME = os.environ.get('NEW_RELIC_APP_NAME', False)
-if NEW_RELIC_LICENSE_KEY and NEW_RELIC_APP_NAME:
-    newrelic.agent.initialize()
-    application = newrelic.agent.wsgi_application()(application)
 
 # Fix django closing connection to memcached after every request (#11331)
 # From https://devcenter.heroku.com/articles/memcachier#django
