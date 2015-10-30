e29d728c73bc4694523c7d093150c7baf308f81a
Bob Silverberg <bob.silverberg@gmail.com>
2015-10-30 09:31:50 -0400

    Update urls to new 1.7/1.8 format

--- oneanddone/base/urls.py
+++ oneanddone/base/urls.py
@@ -2,14 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from django.conf import settings
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from oneanddone.base import views
 
 
-urlpatterns = patterns(
-    '',
-    (r'^(?P<path>contribute\.json)$', 'django.views.static.serve',
-     {'document_root': settings.ROOT}),
+urlpatterns = [
+    url(r'^(?P<path>contribute\.json)$', 'django.views.static.serve',
+        {'document_root': settings.ROOT}),
     url(r'^$', views.HomeView.as_view(), name='base.home'),
-)
+]
--- oneanddone/users/urls.py
+++ oneanddone/users/urls.py
@@ -1,13 +1,12 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from oneanddone.users import views
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^login/$', views.LoginView.as_view(), name='users.login'),
     url(r'^profile/new/$', views.CreateProfileView.as_view(), name='users.profile.create'),
     url(r'^profile/edit/$', views.UpdateProfileView.as_view(), name='users.profile.update'),
@@ -15,4 +14,4 @@ urlpatterns = patterns(
     url(r'^profile/$', views.MyProfileDetailsView.as_view(), name='users.profile.mydetails'),
     url(r'^profile/(?P<id>\d+)/$', views.ProfileDetailsView.as_view(), name='users.profile.details'),
     url(r'^profile/(?P<username>[^/\\]+)/$', views.ProfileDetailsView.as_view(), name='users.profile.details'),
-)
+]
