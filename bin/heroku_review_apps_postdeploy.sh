f82bd5aa86f24179b750ff1426a12cc1b1b12a99
Bob Silverberg <bob.silverberg@gmail.com>
2015-11-21 13:06:37 -0500

    Add Rebecca as a user to review apps

--- bin/heroku_review_apps_postdeploy.sh
+++ bin/heroku_review_apps_postdeploy.sh
@@ -2,3 +2,4 @@
 
 python manage.py migrate
 python manage.py createsuperuser --noinput --username bsilverberg@mozilla.com --email bsilverberg@mozilla.com
+python manage.py createsuperuser --noinput --username rbillings@mozilla.com --email rbillings@mozilla.com
